88d533fc8a086fe84d46bc7caa30d9239600cb3f
envomp <envomp@ttu.ee>
2019-09-05 13:38:18 +0300

    Test from my local branch AsStudent

--- xp_maze/maze.py
+++ xp_maze/maze.py
@@ -2,6 +2,10 @@ from queue import PriorityQueue
 
 
 class MazeSolver:
+
+
+    
+
     def __init__(self, maze_str: str, configuration: dict = None):
         maze = []
         if configuration is None:
