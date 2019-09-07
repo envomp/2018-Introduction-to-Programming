9f504a550a055c2428ea61b2fe33f9062f55209c
envomp@ttu.ee <envomp@ttu.ee>
2019-09-07 19:07:41 +0300

    XP janguru.py

--- xp01_janguru/janguru.py
+++ xp01_janguru/janguru.py
@@ -17,13 +17,16 @@ def meet_me(pos1, jump_distance1, sleep1, pos2, jump_distance2, sleep2):
                 y_current_offset_a = y_base_offset_a - i * alpha_a
                 y_current_offset_b = y_base_offset_b
             x = round((y_current_offset_b - y_current_offset_a) / (alpha_a - alpha_b), 10)
-            y = alpha_a * x + y_base_offset_a
+            y = alpha_b * x + y_base_offset_b
             if (y - pos1) % jump_distance1 == (y - pos2) % jump_distance2 == 0:
                 possible_answers.append(int(y))
         except ZeroDivisionError:
-            print("TODO: PARALLEL BUNNY DRIFTING!")
-    return -1 if possible_answers.__len__() == 0 else min(possible_answers)
+            if y_current_offset_a == y_current_offset_b:
+                print(alpha_b * y_current_offset_a + y_base_offset_b)
+    return -1 if possible_answers.__len__() == 0 else min(
+        filter(lambda xi: xi >= y_base_offset_a and xi >= y_current_offset_b, possible_answers))
 
 
 if __name__ == '__main__':
-    print(meet_me(10, 7, 7, 5, 8, 6))
+    print(meet_me(1, 2, 1, 1, 2, 1))
+    # print(meet_me(0, 15, 6, 5, 5, 3))
