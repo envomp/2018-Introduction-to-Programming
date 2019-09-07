4f286a27ecf360639a0275029683f249c0062012
envomp@ttu.ee <envomp@ttu.ee>
2019-09-07 19:45:20 +0300

    confundled branches

--- xp01_janguru/janguru.py
+++ xp01_janguru/janguru.py
@@ -10,26 +10,25 @@ def meet_me(pos1, jump_distance1, sleep1, pos2, jump_distance2, sleep2):
     for i in range(sleep1 + sleep2):
         try:
             if i >= sleep1:
-                i -= sleep1
                 y_current_offset_a = y_base_offset_a
-                y_current_offset_b = y_base_offset_b - i * alpha_b
+                y_current_offset_b = y_base_offset_b - (i - sleep1) * alpha_b
             else:
                 y_current_offset_a = y_base_offset_a - i * alpha_a
                 y_current_offset_b = y_base_offset_b
             x = round((y_current_offset_b - y_current_offset_a) / (alpha_a - alpha_b), 10)
-            y1 = alpha_a * x + y_base_offset_a
-            y2 = alpha_b * x + y_base_offset_b
-            if (y1 - pos1) % jump_distance1 == (y1 - pos2) % jump_distance2 == 0:
-                possible_answers.append(int(y1))
-            if (y2 - pos1) % jump_distance1 == (y2 - pos2) % jump_distance2 == 0:
-                possible_answers.append(int(y2))
+            if int(x) != x:
+                continue
+            y = alpha_a * x + y_base_offset_a if i >= sleep1 else alpha_b * x + y_base_offset_b
+            if (y - pos1) % jump_distance1 == (y - pos2) % jump_distance2 == 0:
+                possible_answers.append(int(y))
         except ZeroDivisionError:
             if y_current_offset_a == y_current_offset_b:
                 print(alpha_b * y_current_offset_a + y_base_offset_b)
+    print(possible_answers)
     possible_answers = list(filter(lambda xi: xi >= y_base_offset_a and xi >= y_current_offset_b, possible_answers))
     return -1 if possible_answers.__len__() == 0 else min(possible_answers)
 
 
 if __name__ == '__main__':
     print(meet_me(1, 2, 1, 1, 2, 1))
-    print(meet_me(1, 7, 1, 15, 5, 1))
+    print(meet_me(100, 7, 4, 300, 8, 6))
