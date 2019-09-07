70a595d2333dfd020bcd299e6fa99e73d427e066
envomp@ttu.ee <envomp@ttu.ee>
2019-09-07 12:25:59 +0300

    janguru xp

--- xp01_janguru/janguru.py
+++ xp01_janguru/janguru.py
@@ -7,28 +7,24 @@ def meet_me(pos1, jump_distance1, sleep1, pos2, jump_distance2, sleep2):
 
     possible_answers = []
 
-    for i in range(sleep1):
+    for i in range(sleep1 + sleep2):
         try:
-            y_current_offset_a = y_base_offset_a - i * alpha_a
-            y_current_offset_b = y_base_offset_b
+            if i >= sleep1:
+                i -= sleep1
+                y_current_offset_a = y_base_offset_a
+                y_current_offset_b = y_base_offset_b - i * alpha_b
+            else:
+                y_current_offset_a = y_base_offset_a - i * alpha_a
+                y_current_offset_b = y_base_offset_b
             x = round((y_current_offset_b - y_current_offset_a) / (alpha_a - alpha_b), 10)
             y = alpha_a * x + y_base_offset_a
             print(x)
             if (y - pos1) % jump_distance1 == (y - pos2) % jump_distance2 == 0:
                 possible_answers.append(int(y))
-        except Exception:
-            continue
-
-    for j in range(sleep2):
-        try:
-            y_current_offset_a = y_base_offset_a
-            y_current_offset_b = y_base_offset_b - j * alpha_b
-            x = round((y_current_offset_b - y_current_offset_a) / (alpha_a - alpha_b), 10)
-            y = alpha_a * x + y_base_offset_a
-            print(x)
-            if (y - pos1) % jump_distance1 == (y - pos2) % jump_distance2 == 0:
-                possible_answers.append(int(y))
-        except Exception:
-            continue
+        except ZeroDivisionError:
+            print("TODO: PARALLEL BUNNY DRIFTING!")
     print()
     return -1 if possible_answers.__len__() == 0 else min(possible_answers)
+
+
+print(meet_me(10, 7, 7, 5, 8, 6))
