820d5db1e6b15fefb6b3110e4edc144bad5c0214
envomp@ttu.ee <envomp@ttu.ee>
2019-09-07 20:38:07 +0300

    25 line

--- xp01_janguru/janguru.py
+++ xp01_janguru/janguru.py
@@ -17,7 +17,7 @@ def meet_me(pos1, jump_distance1, sleep1, pos2, jump_distance2, sleep2):
             y = alpha_a * x + y_base_offset_a if i >= sleep1 else alpha_b * x + y_base_offset_b
             if (y - pos1) % jump_distance1 == (y - pos2) % jump_distance2 == 0:
                 possible_answers.append(int(y))
-        except ZeroDivisionError:  # TODO
+        except ZeroDivisionError:
             if y_current_offset_a == y_current_offset_b:
                 cycle_mean = int(round(alpha_a * y_current_offset_a + y_current_offset_a, 0))
                 return -1 if cycle_mean > y_base_offset_a and cycle_mean > y_current_offset_b else cycle_mean
