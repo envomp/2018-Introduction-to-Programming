bf96b1e8c6b1d428507eb7265157b0a236344f08
envomp@ttu.ee <envomp@ttu.ee>
2019-09-09 19:26:33 +0300

    primes

--- ex01_geometry/geometry.py
+++ ex01_geometry/geometry.py
@@ -1,16 +1,17 @@
 import math
 
-
-def calculate_area():
-    shape = input("Please insert geometric shape:")
-    dimension = float(input("Please insert radius or side length in cm:"))
-    if shape == "triangle":
-        print(f"The area is {round(dimension**2 / 4 * math.sqrt(3), 2)} cm^2")
-    elif shape == "circle":
-        print(f"The area is {round(3.14 * dimension**2, 2)} cm^2")
-    elif shape == "square":
-        print(f"The area is {round(dimension**2, 2)} cm^2")
-
-
-if __name__ == '__main__':
-    calculate_area()
+shape = input("Please insert geometric shape:")
+if shape == "circle":
+    radius = float(input("Please insert radius in cm:"))
+    area = math.pi * radius**2
+    print(f"The area is {round(area, 2)} cm^2")
+elif shape == "square":
+    length = float(input("Please insert side length in cm:"))
+    area = length**2
+    print(f"The area is {round(area, 2)} cm^2")
+elif shape == "triangle":
+    length = float(input("Please insert side length in cm:"))
+    area = math.sqrt(3) * length**2 / 4
+    print(f"The area is {round(area, 2)} cm^2")
+else:
+    print("Shape is not supported.")
