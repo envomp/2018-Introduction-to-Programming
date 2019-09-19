2ae283cc511f7b8a6aaf5be9a4dfbfb7e7f7a544
envomp@ttu.ee <envomp@ttu.ee>
2019-09-19 17:06:29 +0300

    xp02

--- xp02_conversation/conversation.py
+++ xp02_conversation/conversation.py
@@ -1,178 +1,188 @@
-import random
-import re
-import string
-
-import math
-
-sentence_indices = [["The given number ", "This number ", "Number ", "The aforementioned number "],
-                    [["consists of ", "has ", "is made up of "],
-                     ["involves ", "includes ", "contains ", "is comprised of "]],
-                    [["is ", "occurs to be ", "happens to be "],
-                     ["is not ", "isn't ", "doesn't occur to be ", "doesn't happen to be "]]]
-
-
 class Student:
     def __init__(self, biggest_number: int):
-        self.biggest_number = biggest_number
-        self.possible_answers = set([x for x in range(biggest_number + 1)])
+        """
+        Constructor.
+        save biggest number into a variable that is attainable later on.
+        Create a collection of all possible results [possible_answers] <- dont rename that (can be a list or a set)
+        :param biggest_number: biggest possible number(inclusive) to guess
+        NB: calculating using sets is much faster compared to lists
+        """
+        self.possible_answers = set([all_possible_answers for all_possible_answers in range(biggest_number + 1)])
 
     def decision_branch(self, sentence: str):
-        for start in sentence_indices[0]:
-            if sentence.startswith(start):
-                sentence = sentence.replace(start, '')
-                break
-        if any(sentence.startswith(x) for x in sentence_indices[1][0]):
-            if 'binary' in sentence:
-                if any(x in sentence for x in ['zero', 'zeroes']):
-                    self.number_of_zeroes_to_possible_solutions(int(re.findall(r'(\d)', sentence)[0]))
-                if any(x in sentence for x in ['one', 'ones']):
-                    self.number_of_ones_to_possible_solutions(int(re.findall(r'(\d)', sentence)[0]))
-        elif any(sentence.startswith(x) for x in sentence_indices[1][1]):
-            if 'decimal' in sentence:
-                self.deal_with_random_dec_value(re.findall(r'"(\d)"', sentence)[0])
-            if 'hex' in sentence:
-                self.deal_with_random_hex_value(re.findall(r'"(.)"', sentence)[0])
-            if 'quadratic' in sentence:
-                self.deal_with_quadratic_equation(re.findall(r'"(.+)"', sentence)[0], 'times' in sentence,
-                                                  re.findall(r'(-?\d+\.\d{4})', sentence)[0],
-                                                  'bigger' in sentence)
-        elif any(sentence.startswith(x) for x in sentence_indices[2][1]):
-            if "prime" in sentence:
-                self.deal_with_primes(False)
-            if "composite" in sentence:
-                self.deal_with_composites(False)
-            if "fibonacci" in sentence:
-                self.deal_with_fibonacci_sequence(False)
-            if "catalan" in sentence:
-                self.deal_with_catalan_sequence(False)
-            if 'order' in sentence:
-                self.deal_with_number_order('increasing' in sentence, False)
-        elif any(sentence.startswith(x) for x in sentence_indices[2][0]):
-            if "prime" in sentence:
-                self.deal_with_primes(True)
-            if "composite" in sentence:
-                self.deal_with_composites(True)
-            if "fibonacci" in sentence:
-                self.deal_with_fibonacci_sequence(True)
-            if "catalan" in sentence:
-                self.deal_with_catalan_sequence(True)
-            if 'order' in sentence:
-                self.deal_with_number_order('increasing' in sentence, True)
+        """
+        Regex can and should be used here.
 
-        new_round = list(sorted(self.possible_answers))
-        return f'Possible answers are {new_round}.' if new_round.__len__() > 1 else f'The number I needed to guess was {list(new_round)[0]}.'
+        :param sentence: sentence to solve
+        call one of the functions bellow (within this class) and return either one of the following strings:
+        f"Possible answers are {sorted_list_of_possible_answers_in_growing_sequence)}." if there are multiple possibilities
+        f"The number I needed to guess was {final_answer}." if the result is certain
+        """
+        pass
 
-    def number_of_zeroes_to_possible_solutions(self, amount_of_zeroes: int):
-        self.helper_number_of_ones_and_zeroes('0', amount_of_zeroes)
+    def intersect_possible_answers(self, update: list):
+        """
+        Logical AND between two sets.
 
-    def number_of_ones_to_possible_solutions(self, amount_of_ones: int):
-        self.helper_number_of_ones_and_zeroes('1', amount_of_ones)
+        :param update: new list to be put into conjunction with self.possible_answers
+        conjunction between self.possible_answers and update
+        https://en.wikipedia.org/wiki/Logical_conjunction
+        """
+        pass
 
-    def helper_number_of_ones_and_zeroes(self, what_to_check: str, count: int):
-        self.and_possible_answers(
-            [x for x in range(self.biggest_number + 1) if bin(x).replace('0b', '').count(what_to_check) == count])
+    def exclude_possible_answers(self, update: list):
+        """
+        Logical SUBTRACTION between two sets.
+
+        :param update: new list to be excluded from self.possible_answers
+        update excluded from self.possible_answers
+        """
+        pass
+
+    def deal_with_number_of_zeroes(self, amount_of_zeroes: int):
+        """
+        Filter possible_answers to match the amount of zeroes in its binary form.
+
+        :param amount_of_zeroes: number of zeroes in the correct number's binary form
+        """
+        pass
+
+    def deal_with_number_of_ones(self, amount_of_ones: int):
+        """
+        Filter possible answers to match the amount of ones in its binary form.
+
+        :param amount_of_ones: number of zeroes in the correct number's binary form
+        """
+        pass
 
     def deal_with_primes(self, is_prime: bool):
-        self.and_possible_answers(
-            find_primes_in_range(self.biggest_number)) if is_prime else self.exclusion_possible_answers(
-            find_primes_in_range(self.biggest_number))
+        """
+        Filter possible answers to either keep or remove all primes.
+
+        Call find_primes_in_range to get all composite numbers in range.
+        :param is_prime: boolean whether the number is prime or not
+        """
+        pass
 
     def deal_with_composites(self, is_composite: bool):
-        self.and_possible_answers(
-            find_composites_in_range(self.biggest_number)) if is_composite else self.exclusion_possible_answers(
-            find_composites_in_range(self.biggest_number))
+        """
+        Filter possible answers to either keep or remove all composites.
 
-    def deal_with_random_dec_value(self, decimal_value: str):
-        self.and_possible_answers(
-            [x for x in range(self.biggest_number + 1) if decimal_value in str(x)])
+        Call find_composites_in_range to get all composite numbers in range.
+        :param is_composite: boolean whether the number is composite or not
+        """
+        pass
 
-    def deal_with_random_hex_value(self, hex_value: str):
-        self.and_possible_answers(
-            [x for x in range(self.biggest_number + 1) if hex_value in hex(x).replace('0x', '')])
+    def deal_with_dec_value(self, decimal_value: str):
+        """
+        Filter possible answers to remove all numbers that doesn't have the decimal_value in them.
+
+        :param decimal_value: decimal value within the number like 9 in 192
+        """
+        pass
+
+    def deal_with_hex_value(self, hex_value: str):
+        """
+        Filter possible answers to remove all numbers that doesn't have the decimal_value in them.
+
+        :param decimal_value: hex value within the number like e in fe2
+        """
+        pass
 
     def deal_with_quadratic_equation(self, equation: str, to_multiply: bool, multiplicative: float, is_bigger: bool):
-        a = b = c = 0
-        last = '+'
-        side = False
-        for x in equation.split():
-            if re.match(r'(\d+\w\^2)', x):
-                a += (int(x[:-3]) if last in ['+', '='] else -int(x[:-3])) * (-1 if side else 1)
-            elif re.match(r'(\d+\D$)', x):
-                b += (int(x[:-1]) if last in ['+', '='] else -int(x[:-1])) * (-1 if side else 1)
-            elif re.match(r'(\d+$)', x):
-                c += (int(x) if last in ['+', '='] else -int(x)) * (-1 if side else 1)
+        """
+        Filter possible answers to remove all numbers that doesn't have the decimal_value in them.
 
-            side = side or x == '='
-            last = x
-        g = float(max(quadratic_equation_solver(a, b, c)) if is_bigger else min(quadratic_equation_solver(a, b, c)))
-        if to_multiply:
-            g *= float(multiplicative)
-        else:
-            g /= float(multiplicative)
-        self.deal_with_random_dec_value(str(round(g)))
+        Regex can be used here.
+        Call quadratic_equation_solver with variables a, b, c.
+        deal_with_dec_value should be called.
+        :param equation: the quadratic equation
+        :param to_multiply: whether it is necessary to multiply or divide with a given multiplicative
+        :param multiplicative: the multiplicative to multiply or divide with
+        :param is_bigger: to use the bigger or smaller result of the quadratic equation(min or max from [x1, x2])
+        """
+        pass
 
     def deal_with_fibonacci_sequence(self, is_in: bool):
-        self.and_possible_answers(
-            find_fibonacci_numbers(self.biggest_number)) if is_in else self.exclusion_possible_answers(
-            find_fibonacci_numbers(self.biggest_number))
+        """
+        Filter possible answers to either keep or remove all fibonacci numbers.
+
+        Call find_fibonacci_numbers to get all fibonacci numbers in range.
+        :param is_in: boolean whether the number is in fibonacci sequence or not
+        """
+        pass
 
     def deal_with_catalan_sequence(self, is_in: bool):
-        self.and_possible_answers(
-            find_catalan_numbers(self.biggest_number)) if is_in else self.exclusion_possible_answers(
-            find_catalan_numbers(self.biggest_number))
+        """
+        Filter possible answers to either keep or remove all catalan numbers.
+
+        Call find_catalan_numbers to get all catalan numbers in range.
+        :param is_in: boolean whether the number is in catalan sequence or not
+        """
+        pass
 
     def deal_with_number_order(self, increasing: bool, to_be: bool):
-        self.possible_answers = set(x for x in self.possible_answers if (
-            ''.join(sorted(str(x), reverse=increasing is not True)) == str(x) if to_be else ''.join(
-                sorted(str(x), reverse=increasing is not True)) != str(x)))
+        """
+        Filter possible answers to either keep or remove all numbers with wrong order.
 
-    def and_possible_answers(self, update: list):
-        self.possible_answers = set(self.possible_answers) & set(update)
-
-    def exclusion_possible_answers(self, update: list):
-        self.possible_answers = set(self.possible_answers) - set(update)
+        :param increasing: boolean whether to check is in increasing or decreasing order
+        :param to_be: boolean whether the number is indeed in that order
+        """
+        pass
 
 
 def quadratic_equation_solver(a: int, b: int, c: int):
-    # calculate the discriminant
-    d = (b ** 2) - (4 * a * c)
-    # find two solutions
-    x1 = (-b - math.sqrt(d)) / (2 * a)
-    x2 = (-b + math.sqrt(d)) / (2 * a)
-    return x1, x2
+    """
+    Solve the quadratic equation.
+
+    :param a, b, c: variables for quadratic equation
+    https://en.wikipedia.org/wiki/Quadratic_formula
+    :return: x1, x2, where the discriminant is subtracted from the first one and added to the second one
+    """
+    pass
 
 
 def find_primes_in_range(biggest_number: int):
-    return [num for num in range(2, biggest_number + 1) if not any((num % i) == 0 for i in range(2, num))]
+    """
+    Find all primes in range(end inclusive).
+
+    :param biggest_number: all primes in range of biggest_number(included)
+    https://en.wikipedia.org/wiki/Sieve_of_Eratosthenes
+    :return: list of primes
+    """
+    pass
 
 
 def find_composites_in_range(biggest_number: int):
-    primes = find_primes_in_range(biggest_number)
-    return [num for num in range(2, biggest_number + 1) if num not in primes]
+    """
+    Find all composites in range(end inclusive).
+
+    Call find_primes_in_range from this method to get all composites
+    :return: list of composites
+    :param biggest_number: all composites in range of biggest_number(included)
+    """
+    pass
 
 
 def find_fibonacci_numbers(biggest_number: int):
-    if biggest_number == 0:
-        return [0]
-    fibonacci_numbers = [0, 1]
-    while True:
-        fibonacci_numbers.append(fibonacci_numbers[-1] + fibonacci_numbers[-2])
-        if fibonacci_numbers[-1] > biggest_number:
-            return fibonacci_numbers[:-1]
+    """
+    Find all Fibonacci numbers in range(end inclusive).
+
+    Can be solved using recursion.
+    :param biggest_number: all fibonacci numbers in range of biggest_number(included)
+    https://en.wikipedia.org/wiki/Fibonacci_number
+    :return: list of fibonacci numbers
+    """
+    pass
 
 
 def find_catalan_numbers(biggest_number: int):
-    if biggest_number == 0:
-        return []
-    if biggest_number == 1:
-        return [1]
-    catalan = [1, 1]
-    i = 2
-    # Fill entries in catalan[] using recursive formula
-    while True:
-        catalan.append(0)
-        for j in range(i):
-            catalan[i] = catalan[i] + catalan[j] * catalan[i - j - 1]
-        if catalan[-1] > biggest_number:
-            return catalan[:-1]
-        i += 1
+    """
+    Find all Catalan numbers in range(end inclusive).
+
+    Can be solved using recursion.
+    :param biggest_number: all catalan numbers in range of biggest_number(included)
+    https://en.wikipedia.org/wiki/Catalan_number
+    :return: list of catalan numbers
+    """
+    pass
