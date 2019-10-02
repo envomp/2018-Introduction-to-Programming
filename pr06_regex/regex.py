4f31db84743e110952e7a7674ca2a41a959750e3
envomp@ttu.ee <envomp@ttu.ee>
2019-10-02 23:38:40 +0300

    asd

--- pr06_regex/regex.py
+++ pr06_regex/regex.py
@@ -2,42 +2,15 @@ import re
 
 
 def read_file(path: str) -> list:
-    """
-    Read file and return list of lines read.
-
-    :param path: str
-    :return: list
-    """
-    return [x for x in open(path, "r").readlines()]
+    return [line.rstrip('\n') for line in open(path, encoding="utf8")]
 
 
 def match_specific_string(input_data: list, keyword: str) -> int:
-    """
-    Check if given list of strings contains keyword.
-    Return all keyword occurrences (case insensitive).
-
-    ["Python", "python", "PYTHON", "java"], "python" -> 3
-
-    :param input_data: list
-    :param keyword: str
-    :return: int
-    """
-    return sum([len(re.findall(keyword, x.lower())) for x in input_data])
+    return sum([len(re.findall("(?i)" + keyword, element)) for element in input_data])
 
 
 def detect_email_addresses(input_data: list) -> list:
-    """
-    Check if given list of strings contains valid email addresses.
-    Return all unique valid email addresses in alphabetical order presented in the list.
-
-    ["Test", "Add me test@test.ee", "ago.luberg@taltech.ee", "What?", "aaaaaa@.com", ";_:Ö<//test@test.au??>>>;;d,"] ->
-    ["ago.luberg@taltech.ee", "test@test.au", "test@test.ee"]
-
-    :param input_data: list
-    :return: list
-    """
-    return list(sorted(set(
-        [y.group() for x in input_data for y in re.finditer(r"\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w+)+", x)])))
+    return list(sorted(set([x.group() for y in input_data for x in re.finditer(r"\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w+)+", y)])))
 
 
 if __name__ == '__main__':
