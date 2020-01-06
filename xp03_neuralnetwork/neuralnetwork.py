a513b686eab26700d2c61622d754da5a9ee3f758
envomp@ttu.ee <envomp@ttu.ee>
2020-01-06 13:30:29 +0200

    xp03 git LFS

--- xp03_neuralnetwork/neuralnetwork.py
+++ xp03_neuralnetwork/neuralnetwork.py
@@ -1,295 +1,137 @@
-"""
-XP03 - Neural network.
+"""Neural network. Push again. Try again."""
 
-Functions train(), process_image(), predict() and show_image()
-are taken (and modified) from https://towardsdatascience.com
-A Beginner’s Tutorial on Building an AI Image Classifier using PyTorch
-"""
 import torch
-import torchvision.transforms as transforms
-import torchvision.datasets as datasets
-import torchvision.models as models
+from torchvision import models, transforms
+import torch.utils.data
 import torch.nn as nn
 import torch.optim as optim
-import numpy as np
 from PIL import Image
-import matplotlib.pyplot as plt
-import matplotlib.image as mpimg
-import os
-import logging
 
 
-def train(pic_folder: str, model_name: str):
-    """
-    Train model.
-
-    :param pic_folder:
-    :param model_name: new model name
-    :return: None
-    """
-    # Specify transforms using torchvision.transforms as transforms library
-    transformations = transforms.Compose([
-        transforms.Resize(255),
+normalize_transform = transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
+data_transforms = {
+    'train': transforms.Compose([
+        transforms.RandomRotation(30),
+        transforms.RandomResizedCrop(224),
+        transforms.RandomHorizontalFlip(),
+        transforms.ToTensor(),
+        normalize_transform
+    ]),
+    'eval': transforms.Compose([
+        transforms.Resize(256),
         transforms.CenterCrop(224),
         transforms.ToTensor(),
-        transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
+        normalize_transform
     ])
+}
 
-    train_set = datasets.ImageFolder(pic_folder, transform=transformations)
-    val_set = datasets.ImageFolder(pic_folder, transform=transformations)
 
-    # Put into a Dataloader using torch library
-    train_loader = torch.utils.data.DataLoader(train_set, batch_size=32, shuffle=True)
-    val_loader = torch.utils.data.DataLoader(val_set, batch_size=32, shuffle=True)
+class ImageClassifier:
+    """Image classifier class."""
 
-    # Get pretrained model using torchvision.models as models library
-    model = models.densenet161(pretrained=True)
+    def __init__(self, device, class_count: int):
+        """Initialize classifier instance."""
+        self._device = device
+        self._model = None
+        self._optimizer = None
+        self._criterion = nn.CrossEntropyLoss()
+        self._class_count = class_count
 
-    # Turn off training for their parameters
-    for param in model.parameters():
-        param.requires_grad = False
+    def create(self):
+        """Create pre-trained model."""
+        self._model = models.resnet50(pretrained=True).to(self._device)
 
-    # Create new classifier for model using torch.nn as nn library
-    classifier_input = model.classifier.in_features
-    num_labels = 3
-    classifier = nn.Sequential(nn.Linear(classifier_input, 1024),
-                               nn.ReLU(),
-                               nn.Linear(1024, 512),
-                               nn.ReLU(),
-                               nn.Linear(512, num_labels),
-                               nn.LogSoftmax(dim=1))
-    # Replace default classifier with new classifier
-    model.classifier = classifier
+        for param in self._model.parameters():
+            param.requires_grad = False
 
-    # Find the device available to use using torch library
-    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-    # Move model to the device specified above
-    model.to(device)
+        self._model.fc = nn.Sequential(
+            nn.Linear(self._model.fc.in_features, 128),
+            nn.ReLU(inplace=True),
+            nn.Linear(128, self._class_count)
+        ).to(self._device)
 
-    # Set the error function using torch.nn as nn library
-    criterion = nn.NLLLoss()
-    # Set the optimizer function using torch.optim as optim library
-    optimizer = optim.Adam(model.classifier.parameters())
-    # Load in each dataset and apply transformations using the torchvision.datasets as datasets library
+        self._optimizer = optim.Adam(self._model.fc.parameters())
 
-    epochs = 10
-    for epoch in range(epochs):
-        train_loss = 0
-        val_loss = 0
-        accuracy = 0
+    def save(self, file_path: str):
+        """Save model."""
+        torch.save(self._model.state_dict(), file_path)
 
-        # Training the model
-        model.train()
-        counter = 0
-        for inputs, labels in train_loader:
-            # Move to device
-            inputs, labels = inputs.to(device), labels.to(device)
-            # Clear optimizers
-            optimizer.zero_grad()
-            # Forward pass
-            output = model.forward(inputs)
-            # Loss
-            loss = criterion(output, labels)
-            # Calculate gradients (backpropogation)
+    def load(self, file_path: str):
+        """Load model."""
+        self._model = models.resnet50(pretrained=False).to(self._device)
+
+        self._model.fc = nn.Sequential(
+            nn.Linear(self._model.fc.in_features, 128),
+            nn.ReLU(inplace=True),
+            nn.Linear(128, self._class_count)
+        ).to(self._device)
+
+        self._model.load_state_dict(torch.load(file_path, map_location=self._device))
+        self._optimizer = optim.Adam(self._model.fc.parameters())
+
+    def _train(self, data_loader) -> (float, float):
+        """Training procedure."""
+        self._model.train()
+        running_loss = 0.0
+        running_corrects = 0.0
+
+        for inputs, labels in data_loader:
+            inputs = inputs.to(self._device)
+            labels = labels.to(self._device)
+
+            self._optimizer.zero_grad()
+            outputs = self._model(inputs)
+            loss = self._criterion(outputs, labels)
             loss.backward()
-            # Adjust parameters based on gradients
-            optimizer.step()
-            # Add the loss to the training set's rnning loss
-            train_loss += loss.item() * inputs.size(0)
+            self._optimizer.step()
 
-            # Print the progress of our training
-            counter += 1
-            print(counter, "/", len(train_loader))
+            result = torch.max(outputs, 1)
+            running_loss += loss.detach() * inputs.size(0)
+            running_corrects += torch.sum(result.indices == labels.data).float()
 
-        # Evaluating the model
-        model.eval()
-        counter = 0
-        # Tell torch not to calculate gradients
-        with torch.no_grad():
-            for inputs, labels in val_loader:
-                # Move to device
-                inputs, labels = inputs.to(device), labels.to(device)
-                # Forward pass
-                output = model.forward(inputs)
-                # Calculate Loss
-                valloss = criterion(output, labels)
-                # Add loss to the validation set's running loss
-                val_loss += valloss.item() * inputs.size(0)
+        running_loss /= len(data_loader.dataset)
+        running_corrects /= len(data_loader.dataset)
+        return running_loss, running_corrects
 
-                # Since our model outputs a LogSoftmax, find the real
-                # percentages by reversing the log function
-                output = torch.exp(output)
-                # Get the top class of the output
-                top_p, top_class = output.topk(1, dim=1)
-                # See how many of the classes were correct?
-                equals = top_class == labels.view(*top_class.shape)
-                # Calculate the mean (get the accuracy for this batch)
-                # and add it to the running accuracy for this epoch
-                accuracy += torch.mean(equals.type(torch.FloatTensor)).item()
+    def _eval(self, data_loader) -> (float, float):
+        """Evaluate procedure."""
+        self._model.eval()
+        running_loss = 0.0
+        running_corrects = 0.0
 
-                # Print the progress of our evaluation
-                counter += 1
-                print(counter, "/", len(val_loader))
+        for inputs, labels in data_loader:
+            inputs = inputs.to(self._device)
+            labels = labels.to(self._device)
 
-        # Get the average loss for the entire epoch
-        train_loss = train_loss / len(train_loader.dataset)
-        valid_loss = val_loss / len(val_loader.dataset)
-        # Print out the information
-        print('Accuracy: ', accuracy / len(val_loader))
-        print('Epoch: {} \tTraining Loss: {:.6f} \tValidation Loss: {:.6f}'.format(epoch, train_loss, valid_loss))
+            outputs = self._model(inputs)
+            loss = self._criterion(outputs, labels)
 
-    torch.save(model, model_name)
+            result = torch.max(outputs, 1)
+            running_loss += loss.detach() * inputs.size(0)
+            running_corrects += torch.sum(result.indices == labels.data).float()
 
+        running_loss /= len(data_loader.dataset)
+        running_corrects /= len(data_loader.dataset)
+        return running_loss, running_corrects
 
-def process_image(image_path):
-    """
-    Process image.
+    def train(self, train_loader, eval_loader, num_epochs=3):
+        """Train model."""
+        for epoch in range(num_epochs):
+            print(f"Epoch {epoch + 1}/{num_epochs}")
+            print('-' * 10)
+            epoch_loss, epoch_acc = self._train(train_loader)
+            print(f"Training loss: {epoch_loss:.4f}, accuracy: {epoch_acc:.4f}")
+            epoch_loss, epoch_acc = self._eval(eval_loader)
+            print(f"Validation loss: {epoch_loss:.4f}, accuracy: {epoch_acc:.4f}\n")
 
-    :param image_path: path to image
-    :return: processed image
-    """
-    # Load Image
-    img = Image.open(image_path)
-
-    # Get the dimensions of the image
-    width, height = img.size
-
-    # Resize by keeping the aspect ratio, but changing the dimension so the shortest size is 256px
-    img = img.resize((255, int(255 * (height / width))) if width < height else (int(255 * (width / height)), 255))
-
-    # Get the dimensions of the new image size
-    width, height = img.size
-
-    # Set the coordinates to do a center crop of 224 x 224
-    left = (width - 224) / 2
-    top = (height - 224) / 2
-    right = (width + 224) / 2
-    bottom = (height + 224) / 2
-    img = img.crop((left, top, right, bottom))
-
-    # Turn image into numpy array
-    img = np.array(img)
-
-    # Make the color channel dimension first instead of last
-    img = img.transpose((2, 0, 1))
-
-    # Make all values between 0 and 1
-    img = img / 255
-
-    # Normalize based on the preset mean and standard deviation
-    img[0] = (img[0] - 0.485) / 0.229
-    img[1] = (img[1] - 0.456) / 0.224
-    img[2] = (img[2] - 0.406) / 0.225
-
-    # Add a fourth dimension to the beginning to indicate batch size
-    img = img[np.newaxis, :]
-
-    # Turn into a torch tensor
-    image = torch.from_numpy(img)
-    image = image.float()
-
-    return image
-
-
-def predict(image, model):
-    """
-    Using model to predict the label.
-
-    :param image: image
-    :param model: model name
-    :return:
-    """
-    # Pass the image through our model
-    output = model.forward(image)
-
-    # Reverse the log function in our output
-    output = torch.exp(output)
-
-    # Get the top predicted class, and the output percentage for that class
-    probs, classes = output.topk(1, dim=1)
-
-    return probs.item(), classes.item()
-
-
-def show_image(image):
-    """
-    Show image.
-
-    :param image: Image
-    :return: None
-    """
-    # Convert image to numpy
-    image = image.numpy()
-
-    # Un-normalize the image
-    image[0] = image[0] * 0.226 + 0.445
-
-    # To avoid logging message
-    logger = logging.getLogger()
-    old_level = logger.level
-    logger.setLevel(100)
-
-    # Print the image
-    plt.imshow(np.transpose(image[0], (1, 2, 0)))
-
-    # Turn logging level back
-    logger.setLevel(old_level)
-
-    plt.show()
-
-
-def plot_image(path_to_image: str):
-    """
-    Plot image.
-
-    :param path_to_image: path to image file
-    :return: None
-    """
-    img = mpimg.imread(path_to_image)
-    plt.imshow(img)
-    plt.show()
-
-
-def guess_animal1(image_path: str) -> str:
-    """
-    Read the image file and guess the animal.
-
-    :param image_path: Path to the image
-    :return: Animal species as string
-    """
-    model = torch.load("neuralnetwork.pth")
-    model.eval()
-
-    imsize = 255
-
-    loader = transforms.Compose([
-        transforms.Resize((imsize, imsize), interpolation=Image.NEAREST),
-        # transforms.CenterCrop(224),
-        transforms.ToTensor(),
-        # transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
-    ])
-
-    img = Image.open(image_path)
-
-    """
-    # Proportional resize and crop
-    width, height = img.size
-    img = img.resize((imsize, int(imsize * (height / width))) if width < height 
-                     else (int(imsize * (width / height)), imsize))
-    width, height = img.size
-    left = (width - 224) / 2
-    top = (height - 224) / 2
-    right = (width + 224) / 2
-    bottom = (height + 224) / 2
-    img = img.crop((left, top, right, bottom))
-    """
-
-    image = loader(img).float().unsqueeze(0)
-
-    labels = ["cat", "giraffe", "rabbit"]
-    _, index = torch.max(model(image), 1)
-
-    return labels[index[0]]
+    def predict_one(self, image_path: str) -> int:
+        """Guess single image."""
+        self._model.eval()
+        img = Image.open(image_path)
+        data = data_transforms['eval'](img).to(self._device).float().unsqueeze(0)
+        prediction_tensor = self._model(data)
+        result = torch.max(prediction_tensor, 1)
+        return result.indices[0]
 
 
 def guess_animal(image_path: str) -> str:
@@ -299,33 +141,37 @@ def guess_animal(image_path: str) -> str:
     :param image_path: Path to the image
     :return: Animal species as string
     """
-    animals = ["cat", "giraffe", "rabbit"]
-    model = torch.load("neuralnetwork.pth")
+    labels = ("cat", "giraffe", "rabbit")
+    return labels[guess_animal.classifier.predict_one(image_path)]
 
-    image = process_image(image_path)
-    top_prob, top_class = predict(image, model)
 
-    return animals[top_class]
+guess_animal.classifier = ImageClassifier("cpu", 3)
+guess_animal.classifier.load('neuralnetwork.pth')
 
 
 if __name__ == '__main__':
-    # train("pics", "neuralnetwork.pth")
-
-    path = 'test'
-
-    for entry in os.listdir(path):
-        if os.path.isfile(os.path.join(path, entry)):
-            print(f"{entry} - {guess_animal(os.path.join(path, entry))}")
-            # plot_image(os.path.join(path, entry))
-
     """
-    animals = ["cat", "giraffe", "rabbit"]
-    model = torch.load("neuralnetwork.pth")
+    from torchvision import datasets
 
-    for entry in os.listdir(path):
-        if os.path.isfile(os.path.join(path, entry)):
-            image = process_image(os.path.join(path, entry))
-            top_prob, top_class = predict(image, model)
-            show_image(image)
-            print(f"{entry} - {top_prob * 100} % {animals[top_class]}")
+    train_dir = './images/training'
+    valid_dir = './images/validation'
+
+    image_datasets = {
+        'train': datasets.ImageFolder(train_dir, transform=data_transforms['train']),
+        'eval': datasets.ImageFolder(valid_dir, transform=data_transforms['eval'])
+    }
+
+    data_loaders = {
+        'train': torch.utils.data.DataLoader(image_datasets['train'], batch_size=64, shuffle=True),  # num_workers
+        'eval': torch.utils.data.DataLoader(image_datasets['eval'], batch_size=32),
+    }
+
+    ic = ImageClassifier("cuda:0" if torch.cuda.is_available() else "cpu", len(image_datasets['train'].classes))
+    ic.load('./neuralnetwork.py')
+    ic.train(data_loaders['train'], data_loaders['eval'], 3)
+    ic.save('./neuralnetwork_.pth')
     """
+
+    print(guess_animal("./toy-rabbit.jpg"))
+    print(guess_animal("./toy-cat.jpg"))
+    print(guess_animal("./toy-giraffe.jpg"))
