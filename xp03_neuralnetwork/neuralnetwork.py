f19cf8889c05c15e04b09bdaeaae7b8badd672dd
envomp@ttu.ee <envomp@ttu.ee>
2020-01-06 13:56:13 +0200

    xp03 git LFS

--- xp03_neuralnetwork/neuralnetwork.py
+++ xp03_neuralnetwork/neuralnetwork.py
@@ -25,8 +25,9 @@ data_transforms = {
     ])
 }
 
+
 class ImageClassifier:
-    """Image classifier class............"""
+    """Image classifier class."""
 
     def __init__(self, device, class_count: int):
         """Initialize classifier instance."""
